66feb58ebfc16e6ecee7d8193a0640fe2a3cb205
Josue <darksiders20005@gmail.com>
2021-08-20 12:52:09 -0300

    vscode com vim não é bom

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
 describe('Testes do componente App.js', () => {
   test('Verifica se existe um link "Home"', () => {
     renderWithRouter(<App />);
